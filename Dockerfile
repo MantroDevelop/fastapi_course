135fc1f543d22bd3ec56402e0af41991780e770c
MantroDevelop <trolomansecret@gmail.com>
2026-03-03 21:05:23 +0200

    update

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM python:3.11-slim
+COPY . .
+
+RUN pip install -r requirements.txt
+
+cmd ["uvicorn","main:app","--host","0.0.0.0","--port","80"]
---
+++ main.py
@@ -0,0 +1,18 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("Base is clear")
+    await create_tables()
+    print("Base is ready")
+    yield
+    print("Off....")
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
